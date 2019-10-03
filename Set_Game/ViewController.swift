98da328965f16ae7ab2a16c4bbb494d67c2da9a4
Mohamed <malbgal@gmail.com>
2019-10-02 22:26:36 -0700

    Fixing deal logic

--- Set_Game/ViewController.swift
+++ Set_Game/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         var screenCards = game.deal(NumberofCards: 24)!
         for btn in cardUIButtons {
             let card = screenCards.popLast()!
-            paintButton(basedOn: card, withButton: btn)
+            renderCardDetails(basedOn: card, withButton: btn)
         }
     }
     var game :Set_TheGame = Set_TheGame()
@@ -59,44 +59,40 @@ class ViewController: UIViewController {
         }
     }
     
-    func paintButton(basedOn card:Card, withButton btn:UIButton) {
+    func renderCardDetails(basedOn card:Card, withButton btn:UIButton) {
         var shape :String
         var color :UIColor
+        //assign a shape string
         switch card.shape{
-            case .circle: shape = "⚪️"
-            case.square: shape = "⬜️"
+            case .circle: shape = "⬤"
+            case.square: shape = "⏹"
             case.triangle: shape = "▲"
-            }
+        }
+        //assign a UIColor based on the enum
         switch card.color{
-        case .red: color = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+            case .red: color = UIColor.red
+            case .green: color = UIColor.green
+            case .purple: color = UIColor.purple
         }
         
-        //RESUME HERE, i was making the attributed string for display and verification purposes only. make the attr string like ive cp pasted from his example code, and make sure it renders correctly, then fix the game logic(the logic should work but get out the bugs, guaranteed to be some, have not tested this!) close!
-        btn.setTitle(shape, for: UIControl.State.normal)
-        let atrs: [NSAttributedString.Key:Any] = [
-            .strokeWidth: 7.0
-                .strokeColor:
-            ]
+        //repeat the shape 'count' number of times
+        assert(Int(card.count.rawValue) != nil)
+        let shapeRepeat = Int(card.count.rawValue)!
         
+        if shapeRepeat == 3{
+           shape = shape + shape + shape
+        }else if shapeRepeat == 2{
+            shape += shape
+        }
         
-        
-        
-        
-        
-        let attributes: [NSAttributedString.Key:Any] = [
-            .strokeWidth : 5.0,
-            .strokeColor :  #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-        ]
+        //make the attributed string
+        //btn.setTitle(shape, for: UIControl.State.normal)
+        let attrs: [NSAttributedString.Key:Any] = [.strokeWidth: 7.0, .strokeColor:color]
         //now create the NSAttrString object and give it the attr var as a param
-        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
-        
+        let attributedString = NSAttributedString(string: shape, attributes: attrs)
         //now assign the last let-var to the attrText property of our UI Button
-        flipCountLabel.attributedText = attributedString
+        btn.setAttributedTitle(attributedString, for: UIControl.State.normal)
         
-        
-        
-        
-        btn.setAttributedTitle(strAtr, for: <#T##UIControl.State#>)
         }
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var cardUIButtons: [UIButton]!;
